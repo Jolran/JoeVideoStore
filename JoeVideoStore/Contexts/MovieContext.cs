fb2e563bbd4f9ba25df1dd4c1d3bb874aa1a7de7
Jolran <Joel@ratdogentertainment.se>
2017-12-15 16:16:00 +0100

    db context & Moviemodel added..

---
+++ JoeVideoStore/Contexts/MovieContext.cs
@@ -0,0 +1,10 @@
+﻿using JoeVideoStore.Models;
+using System.Data.Entity;
+
+namespace JoeVideoStore.Contexts
+{
+    public class MovieContext : DbContext
+    {
+        public DbSet<MovieModel> Movies { get; set; }
+    }
+}
---
+++ JoeVideoStore/Models/MovieModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JoeVideoStore.Models
+{
+    public class MovieModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Length { get; set; }
+        public int Rating { get; set; }
+        public string Genre { get; set; }
+    }
+}
