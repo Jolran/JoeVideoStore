2768a16f6157eb4d79795fb7ce0cba6133d873a7
Jolran <Joel@ratdogentertainment.se>
2017-12-16 01:09:59 +0100

    Moved Ratings and Genres Lists to MovieModel

--- JoeVideoStore/Models/MovieModel.cs
+++ JoeVideoStore/Models/MovieModel.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace JoeVideoStore.Models
 {
@@ -14,5 +16,36 @@ namespace JoeVideoStore.Models
         public int Rating { get; set; }
         [Required]
         public string Genre { get; set; }
+
+
+        public static readonly List<SelectListItem> Ratings = new List<SelectListItem>()
+        {
+            new SelectListItem {Text = "1", Value = "1" },
+            new SelectListItem {Text = "2", Value = "2" },
+            new SelectListItem {Text = "3", Value = "3" },
+            new SelectListItem {Text = "4", Value = "4" },
+            new SelectListItem {Text = "5", Value = "5" }
+        };
+
+        public static readonly List<SelectListItem> Genres = new List<SelectListItem>()
+        {
+            new SelectListItem {Text = "Action", Value = "Action" },
+            new SelectListItem {Text = "Adventure", Value = "Adventure" },
+            new SelectListItem {Text = "Carton", Value = "Carton" },
+            new SelectListItem {Text = "Comedy", Value = "Comedy" },
+            new SelectListItem {Text = "Documentary", Value = "Documentary" },
+            new SelectListItem {Text = "Drama", Value = "Drama" },
+            new SelectListItem {Text = "Horror", Value = "Horror" },
+            new SelectListItem {Text = "Musical", Value = "Musical" },
+            new SelectListItem {Text = "Romantic", Value = "Romantic" },
+            new SelectListItem {Text = "Sci-Fi", Value = "Sci-Fi" },
+            new SelectListItem {Text = "Thriller", Value = "Thriller" },
+            new SelectListItem {Text = "Western", Value = "Western" },
+            new SelectListItem {Text = "Other", Value = "Other" }
+        };
+
     }
 }
+
+
+
